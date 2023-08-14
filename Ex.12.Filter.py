10bef85d89ac2a7accdc0b76f6819ca0364ff462
sree santhosh <113322823+Sreesanthosh@users.noreply.github.com>
2023-08-14 08:38:27 +0530

    Update Ex.12.Filter.py

--- Ex.12.Filter.py
+++ Ex.12.Filter.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-path="D:\SEM 6\Comp_vision\Programs\Sample_Images\Sample3.jpg"
+path="C:Downloads\srees\Programs\Sample3.jpg"
 image=cv.imread(path)
 kernel1=np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
 filter1=cv.filter2D(src=image,ddepth=-1,kernel=kernel1)
