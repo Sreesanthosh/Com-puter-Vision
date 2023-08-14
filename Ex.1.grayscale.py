151996328820cdbf021da961aa16a026c8e5a116
sree santhosh <113322823+Sreesanthosh@users.noreply.github.com>
2023-08-14 08:09:20 +0530

    Update Ex.1.grayscale.py

--- Ex.1.grayscale.py
+++ Ex.1.grayscale.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
-path = "D:\SEM 6\Comp_vision\Programs\Sample_Images\Sample1.jpg"
+path = "C:\Programs\Sample.Images\Sample10.jpg"
 img =cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imwrite("Output1.jpg",imgGray)
